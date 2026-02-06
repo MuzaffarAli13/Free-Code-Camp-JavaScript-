46cb5233b92f2d3138fecdccf26d730cc8074985
MuzaffarAli13 </khan@1234>
2026-02-06 23:53:10 +0500

    freecodecamp js practice

---
+++ arrays.js
@@ -0,0 +1,57 @@
+// var ourArray = ["one",1];
+// console.log(ourArray);
+
+// Nested Array
+// var ourArray = [["Bulls",1],["white Sox",23,true]]
+// console.log(ourArray);
+
+
+// Acces Array Data With Index
+// var myArray = [50,60,70];
+// var myData = myArray[0];
+// console.log(myData);
+
+
+// Modify Array Data
+// var myArray = [10,20,30];
+// myArray[1] = 45;
+// console.log(myArray);
+
+// Access Multi-Dimensional Arrays
+// var myArray = [[1,2,3],[4,5,6],[7,8,9],[[10,11,12],1234,45]];
+// var myData = myArray[3][0][0];
+// console.log(myData);
+
+
+// Push();
+//  var myArray = [["Apple","Mango"],["Banana",30]];
+//  myArray.push(["Dog","Cat"]);
+//  console.log(myArray);
+ 
+ 
+// pop()
+// var myArray = [["cat",30],[30,"Apples"]];
+// var removedfromArray = myArray.pop();
+// console.log(myArray);
+
+
+//  Shift();
+// var myArray = ["Muzaffar","Ali","Aftab"];
+// var shiftingArray = myArray.shift();
+// console.log(myArray);
+
+
+// Unshift()
+// var myArray = [["Ali",25],["Khan",36]];
+// myArray.shift() // removed firt Element
+// myArray.unshift(["Muzaffar",18]);
+// console.log(myArray); 
+// [ [ 'Muzaffar', 18 ], [ 'Khan', 36 ] ]
+
+
+
+// Shoping list with arrays
+// var mylist = [["Apples",30],["Eggs",10],["Milk",2],["Bananas",12]];
+// console.log(mylist);
+
+
--- function.js
+++ function.js
@@ -1,9 +1,104 @@
 // Word blanks
-function WordBlanks(myNoun,myAdjective,myVerb,myAdverb){
-    var result = "";
-    result += "The " + myAdjective + " " + myNoun + " " + myVerb + " to the store " + myAdverb;
-    return result  
-}
+// function WordBlanks(myNoun,myAdjective,myVerb,myAdverb){
+//     var result = "";
+//     result += "The " + myAdjective + " " + myNoun + " " + myVerb + " to the store " + myAdverb;
+//     return result  
+// }
 
-console.log(WordBlanks("dog","big","ran","quickly"));
-console.log(WordBlanks("bike","slow","flew","slowly"));
+// console.log(WordBlanks("dog","big","ran","quickly"));
+// console.log(WordBlanks("bike","slow","flew","slowly"));
+
+
+
+// Write Reusable with Functions
+// function reusableFunction() {
+//     console.log("Hello World!");
+// }
+// reusableFunction()
+// reusableFunction()
+
+// Passing values to function with Arguments
+// function functionWithArgs(a,b) {
+//     console.log(a + b);
+// }
+// functionWithArgs(10,5);
+
+
+
+// global Scope and Function
+// var myGlobal = 10;
+// function fun1(){
+//    console.log(myGlobal);
+// }
+// fun1()]
+// console.log(myGlobal);
+
+
+
+// Local Scope And Functon
+// function myLocalFunc(){
+//     var mylocalvar = 5;
+//     console.log(mylocalvar);
+// }
+// myLocalFunc()
+// console.log(mylocalvar); // error variable not found
+
+
+// Global vs Local Scope in Function
+// var outerWear = "T-Shirt";
+// function myOutfit(){
+//     var outerWear = "sweter"
+//     return outerWear
+// }
+// console.log(myOutfit()); // sweter
+// console.log(outerWear); T-Shirt
+
+
+// Return Value from a Function using return keyword
+// function timesFive(num){
+//     return num * 5
+// }
+// console.log(timesFive(5));
+// console.log(timesFive(2));
+
+
+// UnderStanding undefined Value from a Function
+// var sum = 0;
+// function addfive(){
+//     sum += 5;
+// }  // Not return means Undefied 
+
+
+
+// Sum of Multiple Numbers Logic
+// var numbers = [0, 1, 2, 3, 4, 5];
+// function sumArray(arr){
+//     let total = 0;
+//     for (let element of arr){
+//         total += element
+//     }
+//     console.log(total);
+// }
+// sumArray(numbers) 
+
+
+
+// Asignment with a return value
+// var processed = 0;
+// function processedArg(num){
+//     return (num + 5) / 3
+// }
+// processed = processedArg(10);
+// console.log(processed);
+
+
+// Stand In Line   
+function nextInLine(arr,item){
+    arr.push(item) 
+    return arr.shift()
+}
+var testArr = [1,2,3,4,5];
+console.log("Before " + JSON.stringify(testArr));
+console.log(nextInLine(testArr,6));
+console.log("After " + JSON.stringify(testArr));
+// How It work 
